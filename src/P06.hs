5f121401a4b9120811052eaab2cc33672893e3a0
ToF- <cthibaut@octo.com>
2021-12-06 21:42:14 +0100

    day 6

--- src/P06.hs
+++ src/P06.hs
@@ -9,3 +9,15 @@ generation g = map decrease g <> [8 | x <- g, x == 0]
 
 nthGeneration :: [Integer] -> Int -> [Integer]
 nthGeneration g i = (iterate generation g) !! i
+
+tally :: [Integer] -> [Int]
+tally g = map count [0..8]
+    where
+        count x = length (filter (==x) g)  
+
+
+evolve :: [Int] -> [Int]
+evolve [a,b,c,d,e,f,g,h,i] = [b,c,d,e,f,g,h+a,i,a]
+
+nthEvolution :: [Int] -> Int -> [Int]
+nthEvolution t i = (iterate evolve t) !! i
--- test/P06Spec.hs
+++ test/P06Spec.hs
@@ -15,7 +15,7 @@ spec = do
             generation (generation sample) `shouldBe` [1,2,1,6,0,8]
 
     describe "nthGeneration" $ do
-        it "gives the nth generation from initian" $ do
+        it "gives the nth generation from initial" $ do
            sample `nthGeneration` 1 `shouldBe` [2,3,2,0,1]
            sample `nthGeneration` 18 `shouldBe` [6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8]
 
@@ -25,3 +25,30 @@ spec = do
         it "should pass the puzzle" $ do
             length (puzzle `nthGeneration` 80) `shouldBe` 365862
 
+    describe "tally" $ do
+        it "count each timer" $ do
+            tally sample  `shouldBe` [0,1,1,2,1,0,0,0,0]
+
+
+    describe "evolve" $ do
+        it "states the tally after a generation" $ do
+            let t = tally sample 
+            evolve t `shouldBe` [1,1,2,1,0,0,0,0,0]
+
+        it "makes 0s become 8s and add to 6s" $ do
+            let t = tally sample
+            evolve (evolve t) `shouldBe` [1,2,1,0,0,0,1,0,1]
+
+    describe "nthEvolution" $ do
+        it "give the nth evolution from initial tall" $ do
+            (tally sample) `nthEvolution` 1 `shouldBe` [1,1,2,1,0,0,0,0,0] 
+            (tally sample) `nthEvolution` 18 `shouldBe` [3,5,3,2,2,1,5,1,4] 
+
+        it "should pass the sample" $ do
+            sum ((tally sample) `nthEvolution` 80)  `shouldBe` 5934 
+
+        it "should pass the puzzle" $ do
+            sum ((tally puzzle) `nthEvolution` 80) `shouldBe` 365862
+
+        it "should pass the puzzle for 256 generations" $ do
+            sum ((tally puzzle) `nthEvolution` 256) `shouldBe`  1653250886439
