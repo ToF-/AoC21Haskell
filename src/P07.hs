0a3c599d90c6eff81ef5eecd13cb39b7b65d535a
Christophe Thibaut <cthibaut@octo.com>
2021-12-07 08:09:40 +0100

    day 7

---
+++ src/P07.hs
@@ -0,0 +1,19 @@
+module P07
+    where
+
+minimumFuel :: [Integer] -> Integer
+minimumFuel ps = minimum (map fuel [minX..maxX])
+    where
+        minX = minimum ps
+        maxX = maximum ps
+        fuel x = sum (map (\n -> abs (x-n)) ps)
+
+minimumFuel' :: [Integer] -> Integer
+minimumFuel' ps = minimum (map fuel [minX..maxX])
+    where
+        minX = minimum ps
+        maxX = maximum ps
+        fuel x = sum (map (\n -> consumption (abs (x-n))) ps)
+
+        consumption 0 = 0
+        consumption x = sum [0..x]
