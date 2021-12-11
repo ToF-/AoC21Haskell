ea0697d64ee8f3cea6bacdcad4a7efb9a1625bac
Christophe Thibaut <cthibaut@octo.com>
2021-12-11 14:17:16 +0100

    day 11

--- src/P11.hs
+++ src/P11.hs
@@ -2,8 +2,19 @@ module P11
     where
 
 import qualified Data.Map as M
+import qualified Data.List as L
 import Data.Maybe
 
+sample = ["5483143223"
+         ,"2745854711"
+         ,"5264556173"
+         ,"6141336146"
+         ,"6357385478"
+         ,"4167524645"
+         ,"2176841721"
+         ,"6882881134"
+         ,"4846848554"
+         ,"5283751526"]
 type Coord = (Int,Int)
 type Energy = Char
 type Grid = M.Map Coord Energy
@@ -26,7 +37,43 @@ toStrings = map catMaybes . sdrooc
             (maxRow,maxCol) = fst (M.findMax g)
 
 evolve :: Grid -> Grid
-evolve = M.map increase
+evolve = M.map release . flash . M.map increase
     where
-        increase '9' = '0'
-        increase c = succ c
+    increase :: Energy -> Energy
+    increase c | c `elem` "012345678" = succ c
+    increase '9' = '*'
+    increase c   = c
+
+    flash :: Grid -> Grid
+    flash g = if M.null (stars g) then g else ripple g (M.keys (stars g))
+
+    stars :: Grid -> Grid
+    stars = M.filter ((==)'*')
+
+    ripple :: Grid -> [Coord] -> Grid
+    ripple g coords = flash (L.foldl rippleCoord g coords)
+        where
+            rippleCoord :: Grid -> Coord -> Grid
+            rippleCoord g (row,col) = M.adjust (const '.') (row,col) g'
+                where
+                    g' = L.foldl increaseCoord g (adjacents (row,col))
+                    increaseCoord g (row,col) = M.adjust increase (row,col) g
+                    adjacents (i,j) = [(i-1,j-1),(i-1,j),(i-1,j+1),(i,j-1),(i,j+1),(i+1,j-1),(i+1,j),(i+1,j+1)]
+
+    release :: Energy -> Energy
+    release '.' = '0'
+    release c   = c
+
+countFlashes :: Int -> [[Char]] -> Int
+countFlashes n =  sum . L.map (M.size . M.filter (=='0')) . steps n . fromStrings
+
+steps :: Int -> Grid -> [Grid]
+steps n = take (n+1) . iterate evolve
+
+firstStepWithAllFlash :: [[Char]] -> Maybe Int
+firstStepWithAllFlash = findFullFlash 0 . fromStrings
+    where
+        findFullFlash :: Int -> Grid -> Maybe Int
+        findFullFlash 1000 _ = Nothing
+        findFullFlash n g | (M.size (M.filter (=='0') g)) == (M.size g) = Just n
+        findFullFlash n g | otherwise = findFullFlash (succ n) (evolve g)
--- test/P11Spec.hs
+++ test/P11Spec.hs
@@ -4,6 +4,16 @@ module P11Spec
 import Test.Hspec
 import P11
 
+puzzle = ["6744638455"
+         ,"3135745418"
+         ,"4754123271"
+         ,"4224257161"
+         ,"8167186546"
+         ,"2268577674"
+         ,"7177768175"
+         ,"2662255275"
+         ,"4655343376"
+         ,"7852526168"]
 spec :: SpecWith ()
 spec = do
     describe "step" $ do
@@ -18,16 +28,37 @@ spec = do
                                  ,"22222"
                                  ,"22222"
                                  ,"22222"]
-        it "cause octopuses with energy=9 to flash" $ do
-            let grid=["11111"
-                     ,"11111"
-                     ,"11911"
-                     ,"11111"
-                     ,"11111"]
-            step grid `shouldBe` ["11111"
-                                 ,"12221"
-                                 ,"12021"
-                                 ,"12221"
-                                 ,"11111"]
+        it "cause octopuses with energy=9 to flash and their energy to ripple" $ do
+            let grid  =  ["11111"
+                         ,"19991"
+                         ,"19191"
+                         ,"19991"
+                         ,"11111"]
+            let result = ["34543"
+                         ,"40004"
+                         ,"50005"
+                         ,"40004"
+                         ,"34543"]
+            step grid `shouldBe` result
+            step (step grid) `shouldBe` ["45654"
+                                        ,"51115"
+                                        ,"61116"
+                                        ,"51115"
+                                        ,"45654"]
 
+    describe "countFlashes" $ do
+        it "count the flashed octopuses given a number of steps" $ do
+            countFlashes 1 sample `shouldBe` 0
+            countFlashes 2 sample `shouldBe` 35
+            countFlashes 10 sample `shouldBe` 204
+            countFlashes 100 sample `shouldBe` 1656
 
+        it "should pass the puzzle" $ do
+            countFlashes 100 puzzle `shouldBe` 1608
+
+    describe "firstStepWithAllFlashes" $ do
+        it "tells the first step at which all the octopuses flash" $ do
+            firstStepWithAllFlash sample `shouldBe` Just 195
+
+        it "should pass the puzzle" $ do
+            firstStepWithAllFlash puzzle `shouldBe` Just 214
