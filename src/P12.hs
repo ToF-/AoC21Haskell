e760ba43cbfdbfa88a93724ff91fcc831845c96d
ToF- <cthibaut@octo.com>
2021-12-13 02:18:12 -0500

    day 12 a

--- src/P12.hs
+++ src/P12.hs
@@ -11,15 +11,18 @@ data Cave = Small String
 
 type Graph = M.Map Cave [Cave]
 type Connection = (Cave,Cave)
-type Path = ([Cave],[Cave])
+type Path = [Cave]
 
 cave :: String -> Cave
 cave s | isUpper (head s) = Big s
        | otherwise        = Small s
 
 graph :: [String] -> Graph
-graph ss = L.foldl addNode M.empty (L.map connection ss)
+graph ss = L.foldl addNode M.empty $ connections <> flipConnections
     where
+        connections = L.map connection ss
+        flipConnections = L.map (\(a,b) -> (b,a)) connections
+
         connection :: String -> Connection
         connection s = (start,end)
             where
@@ -29,8 +32,27 @@ graph ss = L.foldl addNode M.empty (L.map connection ss)
         addNode :: Graph -> Connection -> Graph
         addNode g (start,end) = M.insertWith (flip (<>)) start [end] g
 
-adjacents :: Graph -> Cave -> [Cave] -> [Cave]
-adjacents g c vs = case M.lookup c g of
-                     Nothing -> error "wrong initial cave"
-                     Just cs -> L.filter (not . (`elem` vs)) cs
+continue :: Graph -> Path -> [Path]
+continue g [] = error "empty path given to continue"
+continue g (Small "end":_) = []
+continue g p = case M.lookup (head p) g of
+                 Nothing -> []
+                 Just cs -> L.map (\c -> c:p) (L.filter visitable cs)
+                    where
+                        visitable (Big _) = True
+                        visitable c = not (c `elem` p)
+
+continuePaths :: Graph -> [Path] -> [Path]
+continuePaths g ps = L.concatMap (continue g) ps 
+
+allPaths :: Graph -> [Path] -> [Path]
+allPaths g ps = converge $ iterate (continuePaths g) ps 
+    where
+        converge :: [[Path]] -> [Path]
+        converge [] = []
+        converge ([]:_) = []
+        converge (ps:pss) = ps <> converge pss
+    
+solutions :: Graph -> [Path]
+solutions g = L.map reverse $ L.filter (\p -> head p == Small "end") $ allPaths g [[Small "start"]]
 
--- test/P12Spec.hs
+++ test/P12Spec.hs
@@ -13,22 +13,104 @@ sample = ["start-A"
          ,"A-end"
          ,"b-end"]
 
+puzzle = ["yb-pi"
+         ,"jg-ej"
+         ,"yb-KN"
+         ,"LD-start"
+         ,"end-UF"
+         ,"UF-yb"
+         ,"yb-xd"
+         ,"qx-yb"
+         ,"xd-end"
+         ,"jg-KN"
+         ,"start-qx"
+         ,"start-ej"
+         ,"qx-LD"
+         ,"jg-LD"
+         ,"xd-LD"
+         ,"ej-qx"
+         ,"end-KN"
+         ,"DM-xd"
+         ,"jg-yb"
+         ,"ej-LD"
+         ,"qx-UF"
+         ,"UF-jg"
+         ,"qx-jg"
+         ,"xd-UF"]
+
 g = graph sample
 spec :: SpecWith ()
 spec = do
     describe "graph" $ do
         it "create a graph from a list of connections" $ do
-            M.toList g `shouldBe` [(Small "b",[Small "d",Small "end"])
+            M.toList g `shouldBe` [(Small "b",[Small "d",Small "end",Small "start",Big "A"])
+                                  ,(Small "c",[Big "A"])
+                                  ,(Small "d",[Small "b"])
+                                  ,(Small "end",[Big "A",Small "b"])
                                   ,(Small "start",[Big "A",Small "b"])
-                                  ,(Big "A",[Small "c",Small "b",Small "end"])]
-    describe "adjacents" $ do
-        it "tell what connections are possible from a cave, given already visited caves" $ do
-            adjacents g (Small "start") [] `shouldBe` [Big "A", Small "b"]
-            adjacents g (Big "A") [Small "b"] `shouldBe` [Small "c", Small "end"]
+                                  ,(Big "A",[Small "c",Small "b",Small "end",Small "start"])]
+    describe "continue" $ do
+        it "tell what possible paths are possible from the last cave of a path" $ do
+            continue g [Small "start"] `shouldBe` [[Big "A", Small "start"]
+                                                  ,[Small "b", Small "start"]]
+            continue g [Small "b", Small "start"] `shouldBe` [[Small "d",Small "b",Small "start"]
+                                                             ,[Small "end",Small "b",Small "start"]
+                                                             ,[Big "A",Small "b",Small "start"]]
+    describe "continuePaths" $ do
+        it "continue paths given a list of paths" $ do
+            let ps = [[Big "A", Small "start"]
+                     ,[Small "b", Small "start"]]
+            continuePaths g ps `shouldBe` [[Small "c",Big "A",Small "start"]
+                                          ,[Small "b",Big "A",Small "start"]
+                                          ,[Small "end",Big "A",Small "start"]
+                                          ,[Small "d",Small "b",Small "start"]
+                                          ,[Small "end",Small "b",Small "start"]
+                                          ,[Big "A",Small "b",Small "start"]]
+    describe "allPaths" $ do
+        it "find all possible paths given an initial list of paths" $ do
+            let ps = [[Small "start"]]
+            allPaths g ps  `shouldBe` [[Small "start"]
+                                      ,[Big "A",Small "start"]
+                                      ,[Small "b",Small "start"]
+                                      ,[Small "c",Big "A",Small "start"]
+                                      ,[Small "b",Big "A",Small "start"]
+                                      ,[Small "end",Big "A",Small "start"]
+                                      ,[Small "d",Small "b",Small "start"]
+                                      ,[Small "end",Small "b",Small "start"]
+                                      ,[Big "A",Small "b",Small "start"]
+                                      ,[Big "A",Small "c",Big "A",Small "start"]
+                                      ,[Small "d",Small "b",Big "A",Small "start"]
+                                      ,[Small "end",Small "b",Big "A",Small "start"]
+                                      ,[Big "A",Small "b",Big "A",Small "start"]
+                                      ,[Small "c",Big "A",Small "b",Small "start"]
+                                      ,[Small "end",Big "A",Small "b",Small "start"]
+                                      ,[Small "b",Big "A",Small "c",Big "A",Small "start"]
+                                      ,[Small "end",Big "A",Small "c",Big "A",Small "start"]
+                                      ,[Small "c",Big "A",Small "b",Big "A",Small "start"]
+                                      ,[Small "end",Big "A",Small "b",Big "A",Small "start"]
+                                      ,[Big "A",Small "c",Big "A",Small "b",Small "start"]
+                                      ,[Small "d",Small "b",Big "A",Small "c",Big "A",Small "start"]
+                                      ,[Small "end",Small "b",Big "A",Small "c",Big "A",Small "start"]
+                                      ,[Big "A",Small "b",Big "A",Small "c",Big "A",Small "start"]
+                                      ,[Big "A",Small "c",Big "A",Small "b",Big "A",Small "start"]
+                                      ,[Small "end",Big "A",Small "c",Big "A",Small "b",Small "start"]
+                                      ,[Small "end",Big "A",Small "b",Big "A",Small "c",Big "A",Small "start"]
+                                      ,[Small "end",Big "A",Small "c",Big "A",Small "b",Big "A",Small "start"]]
 
-    describe "paths" $ do
-        it "tell what possible paths are possible from a cave, given already visited caves" $ do
-            possiblePaths g (Small "start") [] `shouldBe`
-                [([Small "start"], [Small "start", Big "A"])
-                ,([Small "start"], [Small "start", Small "b"])]
+    describe "solutions" $ do
+        it "tell the paths that lead from start to end" $ do
+            solutions g `shouldBe` [[Small "start" ,Big "A",Small "end"]
+                                   ,[Small "start",Small "b",Small "end"]
+                                   ,[Small "start",Big "A",Small "b",Small "end"]
+                                   ,[Small "start",Small "b",Big "A",Small "end"]
+                                   ,[Small "start",Big "A",Small "c",Big "A",Small "end"]
+                                   ,[Small "start",Big "A",Small "b",Big "A",Small "end"]
+                                   ,[Small "start",Big "A",Small "c",Big "A",Small "b",Small "end"]
+                                   ,[Small "start",Small "b",Big "A",Small "c",Big "A",Small "end"]
+                                   ,[Small "start",Big "A",Small "c",Big "A",Small "b",Big "A",Small "end"]
+                                   ,[Small "start",Big "A",Small "b",Big "A",Small "c",Big "A",Small "end"]]
+        it "should pass the sample" $ do
+            length (solutions g) `shouldBe` 10 
+        it "should pass the puzzle" $ do
+            length (solutions (graph puzzle)) `shouldBe` 4011
 
