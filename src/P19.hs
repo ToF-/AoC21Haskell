4368dabad72fa5750b68064dc03ab190889ba8e5
ToF- <cthibaut@octo.com>
2021-12-27 02:20:13 -0500

    P19 wip

--- src/P19.hs
+++ src/P19.hs
@@ -1,21 +1,23 @@
 module P19
     where
 
-import Data.List
+import qualified Data.List as L
+import qualified Data.Set as S
 import Data.Maybe
 
 data Point = Point { x :: Integer
                    , y :: Integer
                    , z :: Integer
-                   , distances :: [Integer] }
+                   , distances :: S.Set Integer }
     deriving (Eq,Ord,Show)
 
 type Distance = Integer
 type Coord = (Integer,Integer,Integer)
 type Scanner = [Point]
+type Rotation = [[Integer]]
 
 point :: Coord -> Point
-point (a,b,c) = Point { x = a, y = b, z = c, distances = [] }
+point (a,b,c) = Point { x = a, y = b, z = c, distances = S.empty }
 
 distance :: Point -> Point -> Distance
 distance p1 p2 = dx2 + dy2 + dz2
@@ -26,16 +28,51 @@ distance p1 p2 = dx2 + dy2 + dz2
         square a = a * a
 
 scan :: [Coord] -> [Point] 
-scan cds = map setDistances pts
+scan cds = L.map setDistances pts
     where
         pts = map point cds
-        setDistances p = p { distances = tail $ sort ds }
+        setDistances p = p { distances = S.fromList ds }
             where
-                ds = map (p `distance`) pts
+                ds = L.map (p `distance`) pts
 
 readScanners :: [String] -> [Scanner]
-readScanners = map (scan . (map toCoord)) . scannerGroups
+readScanners = L.map (scan . (L.map toCoord)) . scannerGroups
     where 
         toCoord s = read ("("<>s<>")")
-        scannerGroups = filter (isCoord . head) . groupBy (\s t -> isCoord s == isCoord t)  
+        scannerGroups = L.filter (isCoord . head) . L.groupBy (\s t -> isCoord s == isCoord t)  
         isCoord s = ',' `elem` s
+
+rotate :: Rotation -> Coord -> Coord
+rotate [[d,e,f],[g,h,i],[j,k,l]] (a,b,c) = (m,n,o)
+    where
+        m = a * d + b * g + c * j
+        n = a * e + b * h + c * k
+        o = a * f + b * i + c * l 
+
+rotations :: Coord -> [Coord]
+rotations c = map (flip rotate c) rs
+    where
+        rs = [[[ 1, 0, 0] ,[ 0, 1, 0] ,[ 0, 0, 1]]
+             ,[[ 1, 0, 0] ,[ 0, 0,-1] ,[ 0, 1, 0]]
+             ,[[ 1, 0, 0] ,[ 0,-1, 0] ,[ 0, 0,-1]]
+             ,[[ 1, 0, 0] ,[ 0, 0, 1] ,[ 0,-1, 0]]
+             ,[[ 0,-1, 0] ,[ 1, 0, 0] ,[ 0, 0, 1]]
+             ,[[ 0, 0, 1] ,[ 1, 0, 0] ,[ 0, 1, 0]]
+             ,[[ 0, 1, 0] ,[ 1, 0, 0] ,[ 0, 0,-1]]
+             ,[[ 0, 0,-1] ,[ 1, 0, 0] ,[ 0,-1, 0]]
+             ,[[ 1, 0, 0] ,[ 0,-1, 0] ,[ 0, 0, 1]]
+             ,[[-1, 0, 0] ,[ 0, 0,-1] ,[ 0,-1, 0]]
+             ,[[-1, 0, 0] ,[ 0, 1, 0] ,[ 0, 0,-1]]
+             ,[[-1, 0, 0] ,[ 0, 0, 1] ,[ 0, 1, 0]]
+             ,[[ 0, 1, 0] ,[-1, 0, 0] ,[ 0, 0, 1]]
+             ,[[ 0, 0, 1] ,[-1, 0, 0] ,[ 0,-1, 0]]
+             ,[[ 0,-1, 0] ,[-1, 0, 0] ,[ 0, 0,-1]]
+             ,[[ 0, 0,-1] ,[-1, 0, 0] ,[ 0, 1, 0]]
+             ,[[ 0, 0,-1] ,[ 0, 1, 0] ,[ 1, 0, 0]]
+             ,[[ 0, 1, 0] ,[ 0, 0, 1] ,[ 1, 0, 0]]
+             ,[[ 0, 0, 1] ,[ 0,-1, 0] ,[ 1, 0, 0]]
+             ,[[ 0,-1, 0] ,[ 0, 0,-1] ,[ 1, 0, 0]]
+             ,[[ 0, 0,-1] ,[ 0,-1, 0] ,[-1, 0, 0]]
+             ,[[ 0,-1, 0] ,[ 0, 0, 1] ,[-1, 0, 0]]
+             ,[[ 0, 0, 1] ,[ 0, 1, 0] ,[-1, 0, 0]]
+             ,[[ 0, 1, 0] ,[ 0, 0,-1] ,[-1, 0, 0]]]
--- test/P19Spec.hs
+++ test/P19Spec.hs
@@ -4,7 +4,8 @@ module P19Spec
 import Test.Hspec
 import P19
 import Sample19Data 
-import Data.List
+import Data.List as L
+import Data.Set as S
 
 spec :: SpecWith ()
 spec = do
@@ -18,14 +19,20 @@ spec = do
         it "tell the distances from a point to every other point in a scanner range" $ do
             let scanner = scan [(2,3,1), (8,-5,0),(4,2,-10)]
             scanner `shouldBe`
-                [Point {x = 2, y = 3, z = 1, distances = [101,126]}
-                ,Point {x = 8, y = -5, z = 0, distances = [101,165]}
-                ,Point {x = 4, y = 2, z = -10, distances = [126,165]}]
+                [Point {x = 2, y = 3, z = 1, distances = fromList [0,101,126]}
+                ,Point {x = 8, y = -5, z = 0, distances = fromList [0,101,165]}
+                ,Point {x = 4, y = 2, z = -10, distances = fromList [0,126,165]}]
 
     describe "readScanners" $ do
         it "read scanners from a file" $ do
             sample <- (readScanners . lines) <$> readFile "test/Sample19Data.txt"
             head (head sample) `shouldBe` 
-                Point {x = 404, y = -588, z = -901, distances = [19429,21242,1004246,1136955,1146209,1154534,1165776,1178580,1292485,1712390,1734501,1795355,2277243,2281254,2401186,3214416,3385779,3759893,3808714,3827748,4390731,5428385,5605830,5937465]}
+                Point {x = 404, y = -588, z = -901, distances = fromList [0,19429,21242,1004246,1136955,1146209,1154534,1165776,1178580,1292485,1712390,1734501,1795355,2277243,2281254,2401186,3214416,3385779,3759893,3808714,3827748,4390731,5428385,5605830,5937465]}
             last (last sample) `shouldBe` 
-                Point {x = 30, y = -46, z = -14, distances = [37469,577669,670424,713284,763982,800379,833333,943704,991242,1004955,1015017,1091606,1125425,1156030,1177106,1184870,1211276,1224629,1277230,1294605,1311389,1313990,1374678,1395301,1445358]}
+                Point {x = 30, y = -46, z = -14, distances = fromList [0,37469,577669,670424,713284,763982,800379,833333,943704,991242,1004955,1015017,1091606,1125425,1156030,1177106,1184870,1211276,1224629,1277230,1294605,1311389,1313990,1374678,1395301,1445358]}
+
+    describe "rotations" $ do
+        it "tell all rotations for a coord" $ do
+            let (x,y,z) = (10,20,30)
+            rotations (x,y,z) `shouldBe`
+                [(x,y,z),(x,z,-y),(x,-y,-z),(x,-z,y),(y,-x,z),(y,z,x),(y,x,-z),(y,-z,-x),(x,-y,z),(-x,-z,-y),(-x,y,-z),(-x,z,y),(-y,x,z),(-y,-z,x),(-y,-x,-z),(-y,z,-x),(z,y,-x),(z,x,y),(z,-y,x),(z,-x,-y),(-z,-y,-x),(-z,-x,y),(-z,y,x),(-z,x,-y)]
